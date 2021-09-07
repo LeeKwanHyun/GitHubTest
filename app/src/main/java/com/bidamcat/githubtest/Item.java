d267e619b526e2d597789301f2722c0a8b767969
kimbidam <zisu55@naver.com>
2021-09-07 10:51:00 +0900

    add subtitle aaa

---
+++ app/src/main/java/com/bidamcat/githubtest/Item.java
@@ -0,0 +1,12 @@
+package com.bidamcat.githubtest;
+
+public class Item {
+
+    String title;
+    int age;
+
+    public Item(String title, int age) {
+        this.title = title;
+        this.age = age;
+    }
+}
--- app/src/main/java/com/bidamcat/githubtest/MainActivity.java
+++ app/src/main/java/com/bidamcat/githubtest/MainActivity.java
@@ -3,18 +3,24 @@ package com.bidamcat.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.tv);
 
         getSupportActionBar().setTitle("GitHub TEST");
         
         int a= 10;
         getSupportActionBar().setSubtitle("sub title : ");
+
+        tv.setText("sample text");
     }
 }
